6ec82827cc076bcc3216dd55cda2546d252afa9e
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2021-12-06 23:21:07 +0100

    Default with bash for linux

--- example-with-local-config-file.sh
+++ example-with-local-config-file.sh
@@ -1,8 +1,8 @@
 # Example about how to call security flavor with bandit, using local repo .mega-linter.yml config,
 # and force an unique linter to be run with ENABLE_LINTERS variable
 
-# ROOT_FOLDER=$PWD
-ROOT_FOLDER="c:/git/demo-megalinter-security-flavor" # If run on windows, replace $PWD by absolute path of the repository root
+ROOT_FOLDER=$PWD
+# ROOT_FOLDER="c:/git/demo-megalinter-security-flavor" # If run on windows, replace $PWD by absolute path of the repository root
 DOCKER_IMAGE="megalinter/megalinter-security:v6-alpha"
 echo "Running MegaLinter on $ROOT_FOLDER using docker image $DOCKER_IMAGE"
 docker run -v "$ROOT_FOLDER:/tmp/lint" -e ENABLE_LINTERS=PYTHON_BANDIT "$DOCKER_IMAGE" 
