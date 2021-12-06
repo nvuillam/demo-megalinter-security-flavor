a4924a21f284100cf24267946e8cdc0e16200e51
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2021-12-06 22:40:16 +0100

    Gitignore & example variables

---
+++ example-with-remote-config-file.sh
@@ -0,0 +1,16 @@
+# Example about how to call security flavor with bandit, using remote .mega-linter.yml config,
+# and force an unique linter to be run with ENABLE_LINTERS variable
+
+# ROOT_FOLDER=$PWD
+ROOT_FOLDER="c:/git/demo-megalinter-security-flavor" # If run on windows, replace $PWD by absolute path of the repository root
+DOCKER_IMAGE="megalinter/megalinter-security:v6-alpha"
+echo "Running MegaLinter on $ROOT_FOLDER using docker image $DOCKER_IMAGE"
+docker run -v "$ROOT_FOLDER:/tmp/lint" \
+    -e MEGALINTER_CONFIG="https://raw.githubusercontent.com/nvuillam/demo-megalinter-security-flavor/main/.mega-linter.yml"\
+    -e ENABLE_LINTERS=PYTHON_BANDIT \
+    "$DOCKER_IMAGE" 
+
+# SARIF file is then available 
+OUTPUT_FILE="$ROOT_FOLDER/megalinter-reports/megalinter-report.sarif"
+echo ""
+echo "SARIF file is available at $OUTPUT_FILE"
