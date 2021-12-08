7f7f8dee5bfebb388772f3878efb728b40563c18
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2021-12-08 21:30:59 +0100

    Test with flavor all

---
+++ java_good_2.java
@@ -0,0 +1,15 @@
+package com.dummy.nico;
+
+@SuppressWarnings("checkstyle:hideutilityclassconstructor")
+public class Application {
+
+  /**
+   * main.
+   *
+   * @param args
+   */
+  public static void main(final String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
