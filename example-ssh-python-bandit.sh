1ab2cc9c8544e5c82db12ee729fe8f619f09edb6
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2021-12-30 01:09:19 +0100

    Updated example :)

--- example-ssh-python-bandit.sh
+++ example-ssh-python-bandit.sh
@@ -2,14 +2,17 @@
 # Example about how to call python bandit using its single-linter megalinter docker image
 
 # DEFINE SCRIPT VARIABLES (you can do the same in your script)
-ROOT_FOLDER="D:/git" # Always put an absolute path here. Must be the root of all your repositories
+ROOT_FOLDER="C:/git" # Always put an absolute path here. Must be the root of all your repositories
+LOCAL_SSH_FOLDER="C:/Users/33614/.ssh"
+LOCAL_REPORTS_FOLDER="C:/Users/33614/.megaLinter-reports"
 DOCKER_IMAGE="megalinter/megalinter-only-python_bandit:v6-alpha"
 LINTER_NAME="PYTHON_BANDIT"
 WORKSPACE_TO_LINT="demo-megalinter-security-flavor" #name of the folder you want to lint within root folder
 
+set -eu
 # REMOVE PREVIOUS TEST CONTAINERS
 echo "Removing previous tests containers..."
-docker rm --force "$(docker ps --filter name=megalinter-ssh-PYTHON_BANDIT -q)" || true
+docker rm --force "$(docker ps --all --filter name=megalinter-ssh-PYTHON_BANDIT -q)" || true
 echo ""
 # docker container prune --filter name=megalinter-sshd --force
 
@@ -27,6 +30,8 @@ docker run \
        -p 1984:22 \
        --name "megalinter-ssh-$LINTER_NAME" \
        -v "$ROOT_FOLDER:/tmp/lint" \
+       -v "$LOCAL_REPORTS_FOLDER:/tmp/report" \
+       -v "$LOCAL_SSH_FOLDER:/root/docker_ssh" \
        -e MEGALINTER_SSH="true" \
        -d \
        "$DOCKER_IMAGE"
@@ -42,7 +47,4 @@ docker ps
 echo ""
 
 echo "- Connect via ssh: ssh root@localhost -p 1984"
-echo "- input password 'root'"
-echo "- run tmux"
-echo "- run printenv: Dockerfile env variables are visible"
-echo "- run DEFAULT_WORKSPACE=/tmp/lint/${WORKSPACE_TO_LINT} python -m megalinter.run"
+echo "- follow instructions :)"
